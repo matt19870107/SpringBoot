c645372bf31a00bfb8dbfe9b803ceb235b0ba08a
ASUS <ASUS@192.168.0.103>
2017-08-30 21:28:05 +0800

    Properties resolve

--- src/main/java/com/ma/boot/Application.java
+++ src/main/java/com/ma/boot/Application.java
@@ -1,20 +1,38 @@
 package com.ma.boot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ma.boot.config.AuthorSettings;
+
 @RestController
 @SpringBootApplication
 public class Application {
+	@Value("${book.author}")
+	private String bookAutor;
+	@Value("${book.name}")
+	private String bookName;
+	@Autowired
+	AuthorSettings authorSettings;
 	
 	@RequestMapping("/")
 	String index() {
-		return "Hello Spring Boot!";
+		return "book name is:" + bookName +", book autor is:" + bookAutor;
+	}
+	
+	@RequestMapping("/authorsettings")
+	String authorSettings() {
+		return "author name is:" + authorSettings.getName() + ", author age is:" + authorSettings.getAge() + ", author sex is:" + authorSettings.getSex();
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
+		//app.setBannerMode(Banner.Mode.OFF);// turn off the banner
+		app.run(args);
 	}
 }
---
+++ src/main/java/com/ma/boot/config/AuthorSettings.java
@@ -0,0 +1,32 @@
+package com.ma.boot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "author")
+@PropertySource("classpath:author.properties")
+public class AuthorSettings {
+	private String name;
+	private Long age;
+	private String sex;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getAge() {
+		return age;
+	}
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+}
